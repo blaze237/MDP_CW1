04e44de3b9a60ed211aee1b0ca53192778940862
Dan <psydrw@nottingham.ac.uk>
2017-10-25 19:08:15 +0100

    Colour sliders

--- app/src/main/java/com/example/psydrw/mdp_cw1/ColourSelection.java
+++ app/src/main/java/com/example/psydrw/mdp_cw1/ColourSelection.java
@@ -1,14 +1,112 @@
 package com.example.psydrw.mdp_cw1;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
 
 public class ColourSelection extends AppCompatActivity {
+
+    SeekBar redBar;
+    SeekBar greenBar;
+    SeekBar blueBar;
+    View colourPreview;
+
+    int r,g,b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colour_selection);
+
+        redBar =(SeekBar) findViewById(R.id.redBar);
+        greenBar =(SeekBar) findViewById(R.id.greenBar);
+        blueBar =(SeekBar) findViewById(R.id.blueBar);
+
+        colourPreview = (View)findViewById(R.id.colourPreview);
+        colourPreview.setBackgroundColor(Color.argb(255, r, g, b));
+
+        int colour =  getIntent().getIntExtra("Current Colour",0xff000000);
+
+        //Extract rgb vals from colour
+        r = (colour >> 16) & 0xff;
+        g = (colour >> 8) & 0xff;
+        b = colour & 0xff;
+
+        //Init scroll bars
+        redBar.setProgress(r);
+        greenBar.setProgress(g);
+        blueBar.setProgress(b);
+
+        //Set up anonymous listener to grab data from the colour sliders
+        redBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                   r = progress;
+                    colourPreview.setBackgroundColor(Color.argb(255, r, g, b));
+                }
+
+              @Override
+              public void onStartTrackingTouch(SeekBar seekBar) {
+              }
+
+              @Override
+              public void onStopTrackingTouch(SeekBar seekBar) {
+              }
+
+          });
+
+        greenBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    g = progress;
+                    colourPreview.setBackgroundColor(Color.argb(255, r, g, b));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+
+            });
+
+        blueBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                b = progress;
+                colourPreview.setBackgroundColor(Color.argb(255, r, g, b));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+        });
+
+    }
+
+
+
+    public void returnMain(View view)
+    {
+        Intent result = new Intent();
+
+        int colour = Color.argb(255, r, g, b);
+
+        result.putExtra("New Colour",colour);
+        setResult(Activity.RESULT_OK, result);
+        finish();
     }
 }
--- app/src/main/java/com/example/psydrw/mdp_cw1/MainActivity.java
+++ app/src/main/java/com/example/psydrw/mdp_cw1/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.psydrw.mdp_cw1;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int COLOUR_REQUEST = 1;
+    static final int BRUSH_REQUEST = 2;
+
     private FingerPainterView fView;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     //   fView = (FingerPainterView)findViewById(R.id.fingerview);
+        fView = (FingerPainterView)findViewById(R.id.fingerview);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == Activity.RESULT_OK)
+        {
+            //Set new brush colour
+            if (requestCode == COLOUR_REQUEST)
+            {
+                int col = data.getIntExtra("New Colour",0xff000000);
+                fView.setColour(col);
+            }
+        }
     }
 
 
@@ -27,13 +48,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void SetColour(View view) {
-        Intent colScreen = new Intent(getApplicationContext(),ColourSelection.class);
-        colScreen.putExtra("Colour Current",fView.getColour());
+    public void SetColour(View view)
+    {
+        Intent colScreen = new Intent(MainActivity.this,ColourSelection.class);
+        colScreen.putExtra("Current Colour",fView.getColour());
 
-        startActivityForResult(colScreen,2); //Arb int
-
+        startActivityForResult(colScreen,COLOUR_REQUEST); //Arb int
     }
 
 }
-//android:text="@string/btn_Example" /> string resource syntax
